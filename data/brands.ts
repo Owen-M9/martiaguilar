dfe25cd494f0f2e017ed9209f37144ab645e7a98
Owen Masson <owenmasson2014@gmail.com>
2026-08-25 21:14:05 -0300

    feat: se agregaron los logos reales de las marcas

--- data/brands.ts
+++ data/brands.ts
@@ -6,18 +6,24 @@ export interface Brand {
 
 export const brandRows: Brand[][] = [
     [
-        { id: 1, name: "Marca 1", logo: "/images/Johnnie-Walker-logo.jpeg" },
-        { id: 2, name: "Marca 2", logo: "/images/Johnnie-Walker-logo.jpeg" },
-        { id: 3, name: "Marca 3", logo: "/images/Johnnie-Walker-logo.jpeg" },
+        { id: 1, name: "D.F. Entertainment", logo: "/images/brands/DF-Entertainment-logo.jpg" },
+        { id: 2, name: "All Access", logo: "/images/brands/All-Access-logo.png" },
+        { id: 3, name: "Warner Music Group", logo: "/images/brands/Warner-Music-Group-logo.png" },
+        { id: 4, name: "Rolling Stone", logo: "/images/brands/Rolling-Stone-logo.jpeg" },
+        { id: 5, name: "TikTok", logo: "/images/brands/TikTok-logo.png" },
+        { id: 6, name: "Winclap", logo: "/images/brands/Winclap-logo.png" },
     ],
     [
-        { id: 4, name: "Marca 4", logo: "/images/Johnnie-Walker-logo.jpeg" },
-        { id: 5, name: "Marca 5", logo: "/images/Johnnie-Walker-logo.jpeg" },
-        { id: 6, name: "Marca 6", logo: "/images/Johnnie-Walker-logo.jpeg" },
-        { id: 7, name: "Marca 7", logo: "/images/Johnnie-Walker-logo.jpeg" },
+        { id: 7, name: "Blaquè", logo: "/images/brands/Blaque-logo.png" },
+        { id: 8, name: "Cerveza Corona", logo: "/images/brands/Cerveza-Corona-logo.png" },
+        { id: 9, name: "Temu", logo: "/images/brands/Temu-logo.png" },
+        { id: 10, name: "Lollapalooza Argentina", logo: "/images/brands/Lollapalooza-Argentina-logo.png" },
     ],
     [
-        { id: 8, name: "Marca 8", logo: "/images/Johnnie-Walker-logo.jpeg" },
-        { id: 9, name: "Marca 9", logo: "/images/Johnnie-Walker-logo.jpeg" },
-        { id: 10, name: "Marca 10", logo: "/images/Johnnie-Walker-logo.jpeg" },
+        { id: 11, name: "COSRX", logo: "/images/brands/COSRX-logo.png" },
+        { id: 12, name: "LATAM Airlines", logo: "/images/brands/LATAM-Airlines-logo.png" },
+        { id: 13, name: "Mostaza", logo: "/images/brands/Mostaza-logo.png" },
+        { id: 14, name: "Nestlé Argentina", logo: "/images/brands/Nestle-Argentina-logo.png" },
+        { id: 15, name: "Miniso Argentina", logo: "/images/brands/Miniso-Argentina-logo.png" },
+        { id: 16, name: "Johnnie Walker", logo: "/images/brands/Johnnie-Walker-logo.jpeg" },
     ]];
